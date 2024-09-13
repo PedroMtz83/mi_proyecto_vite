c3e5c71a0dde6e1de648f1f304a4c2a511eb5451
Pedro Martinez <pittermartinez9@gmail.com>
2024-09-12 18:52:32 -0700

    nota 4.1

---
+++ methods/MyDataTypes.jsx
@@ -0,0 +1,23 @@
+function MyDataTypes() {
+   
+    let IdNombre = 'Frank';
+
+    IdNombre = 706;
+
+    console.log("IdNombre:", IdNombre);
+
+ 
+    return (
+        <>
+            <div>
+                <h1>
+                    Programa para conocer diferentes Tipos de de Datos
+                </h1>
+                <h1>
+                    {IdNombre}
+                </h1>
+            </div>
+       
+        </>
+    );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 /* import App from './App.jsx' */
 import './index.css'
-import MyFunctions from './functions/MyFunctions.jsx'
-import MyPromises from './functions/MyPromises.jsx'
-import MyPromisesAll from './functions/MyPromisesAll.jsx'
-import MyPromisesRace from './functions/MyPromiseRace.jsx'
-import MyFetchsAPIs from './functions/MyFetchs.jsx'
-import MyFetchsAPIsGET from './functions/MyFetchsGET.jsx'
-import MyFetchsAPIsPost from './functions/MyFetchPost.jsx'
-import MyFetchAPIsBlob from './functions/MyFetchBlob.jsx'
-import MyFetchAPIsPUT from './functions/MyFetchPut.jsx'
+import MyDataTypes from '../methods/MyDataTypes'
+//import MyFunctions from './functions/MyFunctions.jsx'
+//import MyPromises from './functions/MyPromises.jsx'
+//import MyPromisesAll from './functions/MyPromisesAll.jsx'
+//import MyPromisesRace from './functions/MyPromiseRace.jsx'
+//import MyFetchsAPIs from './functions/MyFetchs.jsx'
+//import MyFetchsAPIsGET from './functions/MyFetchsGET.jsx'
+//import MyFetchsAPIsPost from './functions/MyFetchPost.jsx'
+//import MyFetchAPIsBlob from './functions/MyFetchBlob.jsx'
+//import MyFetchAPIsPUT from './functions/MyFetchPut.jsx'
+import MyDataTypes from '../methods/MyDataTypes'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -21,7 +23,8 @@ createRoot(document.getElementById('root')).render(
     {/* <MyPromisesRace></MyPromisesRace> */}
     {/* <MyFetchsAPIsGET></MyFetchsAPIsGET> */}
     {/* <MyFetchsAPIsPost></MyFetchsAPIsPost> */}
-    <MyFetchAPIsPUT></MyFetchAPIsPUT>
+    {/* <MyFetchAPIsPUT></MyFetchAPIsPUT> */}
     {/* <MyFetchAPIsBlob></MyFetchAPIsBlob> */}
+    <MyDataTypes/>
   </StrictMode>,
 )
